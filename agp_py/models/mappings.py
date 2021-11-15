ae32314f085052947fb6bc43f7938565c4142365
Stiggu <abelmunda@hotmail.com>
2021-11-15 01:37:18 -0400

    changed lazy list to dict loops

--- agp_py/models/mappings.py
+++ agp_py/models/mappings.py
@@ -40,75 +40,74 @@ ClassMap = {
 
 # Axie Color Mapping
 ColorMap = {
-    AxieClass.BEAST: {x: y for x, y in [
-        ['0010', 'ffec51'],
-        ['0011', 'ffa12a'],
-        ['0100', 'f0c66e'],
-        ['0110', '60afce'],
-        ['0000', 'ffffff']
-    ]
+    AxieClass.BEAST: {
+        '0010': 'ffec51',
+        '0011': 'ffa12a',
+        '0100': 'f0c66e',
+        '0110': '60afce',
+        '0000': 'ffffff'
                       },
 
-    AxieClass.BUG: {x: y for x, y in [
-        ['0010', 'ff7183'],
-        ['0011', 'ff6d61'],
-        ['0100', 'f74e4e'],
-        ['0000', 'ffffff'],
-    ]},
+    AxieClass.BUG: {
+        '0010': 'ff7183',
+        '0011': 'ff6d61',
+        '0100': 'f74e4e',
+        '0000': 'ffffff'
+    },
 
-    AxieClass.BIRD: {x: y for x, y in [
-        ['0010', 'ff9ab8'],
-        ['0011', 'ffb4bb'],
-        ['0100', 'ff778e'],
-        ['0000', 'ffffff'],
-    ]},
+    AxieClass.BIRD: {
+        '0010': 'ff9ab8',
+        '0011': 'ffb4bb',
+        '0100': 'ff778e',
+        '0000': 'ffffff'
+    },
 
-    AxieClass.PLANT: {x: y for x, y in [
-        ['0010', 'ccef5e'],
-        ['0011', 'efd636'],
-        ['0100', 'c5ffd9'],
-        ['0000', 'ffffff'],
-    ]},
+    AxieClass.PLANT: {
+        '0010': 'ccef5e',
+        '0011': 'efd636',
+        '0100': 'c5ffd9',
+        '0000': 'ffffff'
+    },
 
-    AxieClass.AQUATIC: {x: y for x, y in [
-        ['0010', '4cffdf'],
-        ['0011', '2de8f2'],
-        ['0100', '759edb'],
-        ['0110', 'ff5a71'],
-        ['0000', 'ffffff'],
-    ]},
+    AxieClass.AQUATIC: {
+        '0010': '4cffdf',
+        '0011': '2de8f2',
+        '0100': '759edb',
+        '0110': 'ff5a71',
+        '0000': 'ffffff'
+    },
 
-    AxieClass.REPTILE: {x: y for x, y in [
-        ['0010', 'fdbcff'],
-        ['0011', 'ef93ff'],
-        ['0100', 'f5e1ff'],
-        ['0110', '43e27d'],
-        ['0000', 'ffffff'],
-    ]},
+    AxieClass.REPTILE: {
+        '0010': 'fdbcff',
+        '0011': 'ef93ff',
+        '0100': 'f5e1ff',
+        '0110': '43e27d',
+        '0000': 'ffffff'
+    },
 
-    AxieClass.MECH: {x: y for x, y in [
-        ['0010', 'D9D9D9'],
-        ['0011', 'D9D9D9'],
-        ['0100', 'D9D9D9'],
-        ['0110', 'D9D9D9'],
-        ['0000', 'ffffff'],
-    ]},
+    AxieClass.MECH: {
+        '0010': 'D9D9D9',
+        '0011': 'D9D9D9',
+        '0100': 'D9D9D9',
+        '0110': 'D9D9D9',
+        '0000': 'ffffff'
+    },
 
-    AxieClass.DUSK: {x: y for x, y in [
-        ['0010', 'D9D9D9'],
-        ['0011', 'D9D9D9'],
-        ['0100', 'D9D9D9'],
-        ['0110', 'D9D9D9'],
-        ['0000', 'ffffff'],
-    ]},
+    AxieClass.DUSK: {
+        '0010': 'D9D9D9',
+        '0011': 'D9D9D9',
+        '0100': 'D9D9D9',
+        '0110': 'D9D9D9',
+        '0000': 'ffffff'
+    },
 
-    AxieClass.DAWN: {x: y for x, y in [
-        ['0010', 'D9D9D9'],
-        ['0011', 'D9D9D9'],
-        ['0100', 'D9D9D9'],
-        ['0110', 'D9D9D9'],
-        ['0000', 'ffffff'],
-    ]},
+    AxieClass.DAWN: {
+        '0010': 'D9D9D9',
+        '0011': 'D9D9D9',
+        '0100': 'D9D9D9',
+        '0110': 'D9D9D9',
+        '0000': 'ffffff'
+    },
 }
 
 
